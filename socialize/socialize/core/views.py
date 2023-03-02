abd8771112805005451bd45de9ca2a16e3a7d0e3
lakshyarawat1 <lakshya.paramount@gmail.com>
2023-03-02 21:03:41 +0530

    created like dislike functionality

--- socialize/socialize/core/views.py
+++ socialize/socialize/core/views.py
@@ -112,4 +112,21 @@ def upload(request) :
     
 @login_required(login_url='login')
 def like(request) :
-    return HttpResponse('working')
+    username = request.user.username
+    post_id = request.GET.get('post_id')
+    
+    post = Post.objects.get(id=post_id)
+    
+    like_filter = LikePost.objects.filter(post_id=post_id, username= username).first()
+    if like_filter == None :
+        new_like = LikePost.objects.create(post_id = post_id, username=username)
+        new_like.save()
+        post.no_of_likes = post.no_of_likes+1
+        post.save()
+        return redirect('/')
+    
+    else :
+        like_filter.delete()
+        post.no_of_likes = post.no_of_likes-1
+        post.save()
+        return redirect('/')
